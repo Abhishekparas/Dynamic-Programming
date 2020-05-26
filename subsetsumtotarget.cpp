38ae0d3499b6c48c717fe217f77356d88447ac97
Abhishek Parashar <54927166+Abhishekparas@users.noreply.github.com>
2020-05-26 21:43:58 +0530

    Add files via upload

---
+++ subsetsumtotarget.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool subsetsumtotarget(int* arr, int n, int sum) {
+	bool dp[n + 1][sum + 1];
+
+	for (int i = 0; i < n + 1; ++i)
+	{
+		for (int j = 0; j < sum + 1; ++j)
+		{
+			if (i == 0) {
+				dp[i][j] = false;
+			}
+			if (j == 0) {
+				dp[i][j] = true;
+			}
+		}
+	}
+
+	for (int i = 1; i < n + 1; ++i)
+	{
+		for (int j = 1; j < sum + 1; ++j)
+		{
+			if (arr[i - 1] <= j) {
+				dp[i][j] = dp[i - 1][j - arr[i - 1]] or dp[i - 1][j];
+			}
+			else if (arr[i - 1] > j) {
+				dp[i][j] = dp[i - 1][j];
+			}
+		}
+	}
+	return dp[n][sum];
+}
+
+int main(int argc, char const *argv[])
+{
+	int n, sum;
+	int arr[100];
+	cin >> n >> sum;
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> arr[i];
+	}
+
+	if (subsetsumtotarget(arr, n, sum)) {
+		cout << "Yes" << endl;
+	}
+	else {
+		cout << "No" << endl;
+	}
+	return 0;
+}
